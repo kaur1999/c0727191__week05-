fa6426e6f059a82e0e1dc9dff390c70a4c54216c
visualstudio <visualstudio@122-PC37>
2019-02-08 20:22:43 -0500

    commit 8:00

--- c0727191__week05-/Program.cs
+++ c0727191__week05-/Program.cs
@@ -6,10 +6,125 @@ using System.Threading.Tasks;
 
 namespace c0727191__week05_
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
+
+            Elevator e = new Elevator();
+
+            e.setup();
+
+            e.TraverseList();
+
         }
+
     }
+
+
+
+    class Node
+
+    {
+
+        public Node() { }
+
+        public Node floor;
+
+        public Node elevatorUp;
+
+        public string FloorNumber;
+
+    }
+
+
+
+    class Elevator
+
+    {
+
+        Node Head;
+
+        Node FirstFloor;
+
+        Node SecondFloor;
+
+        Node ThirdFloor;
+
+        Node FourthFloor;
+
+
+
+        public void setup()
+
+        {
+
+            FirstFloor = new Node();
+
+            SecondFloor = new Node();
+
+            ThirdFloor = new Node();
+
+            FourthFloor = new Node();
+
+            Head = FirstFloor;
+
+            FirstFloor.FloorNumber = "First Floor";
+
+            FirstFloor.elevatorUp = SecondFloor;
+
+            SecondFloor.FloorNumber = "Second Floor";
+
+            SecondFloor.elevatorUp = ThirdFloor;
+
+            ThirdFloor.FloorNumber = "Third Floor";
+
+            ThirdFloor.elevatorUp = FourthFloor;
+
+            FourthFloor.FloorNumber = "Fourth Floor";
+
+            FourthFloor.elevatorUp = null;
+
+        }
+
+
+
+        public void TraverseList()
+
+        {
+
+            Node temp;
+
+            temp = Head;
+
+
+
+            // where am I going to start?
+
+            Console.WriteLine("The first floor is " + Head.FloorNumber);
+
+            while (temp != null)
+
+            {
+
+                // NOW GET TO THE SECOND FLOOR!!!!
+
+                temp = temp.elevatorUp;
+
+                Console.WriteLine(temp.FloorNumber);
+
+
+
+            }
+
+
+
+        }
+
+    }
+
 }
